a3a6161a0125d5dea167778ff71b4bb7ddb48eec
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2023-09-07 22:16:07 -0300

    Projeto em desenvolvimento

---
+++ src/app/components/Menu.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Menu = () => {
+  return (
+    <div>
+         <svg height="24" viewBox="0 0 24 24" width="24" focusable="false"  fill='white'
+        ><path d="M21 6H3V5h18v1zm0 5H3v1h18v-1zm0 6H3v1h18v-1z"></path>
+        </svg>
+
+    </div>
+  )
+}
--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,18 +1,41 @@
 import React from 'react'
 import Link from 'next/link'
-
+import { Menu} from "../components/Menu";
+import {LogoYoutube} from "../components/LogoYoutube";
+import { useSelector,useDispatch } from 'react-redux';
+import { changeBoolean } from '../../../Redux/features/sidebarSlice';
 
 export const Sidebar = () => {
 
+  const dispatch = useDispatch();
+  const toggleSidebar = () => {
+    dispatch(changeBoolean());
+  };
 // style="pointer-events: none; display: block; width: 100%; height: 100%;"
   return (
     <main
     className='flex flex-col'
     >
       <section
+    className='flex flex-row m-4 items-center gap-7'
+
+      >
+        <button
+        onClick={toggleSidebar}
+        >
+
+      <Menu></Menu>
+        </button>
+      <LogoYoutube></LogoYoutube>
+      </section>
+
+
+      <section
       className='flex flex-row gap-4 m-3 hover:bg-[#272727]/40 p-3  
        rounded-xl cursor-pointer'
       >
+
+        
       <svg enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" focusable="false" fill='white'>
       <path d="m12 4.44 7 6.09V20h-4v-6H9v6H5v-9.47l7-6.09m0-1.32-8 6.96V21h6v-6h4v6h6V10.08l-8-6.96z"></path></svg>
       <span>
